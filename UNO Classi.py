75d32511c88f6424015f59e37851a8fb2f0cc971
Jacopo230201 <103194628+Jacopo230201@users.noreply.github.com>
2022-04-07 14:58:08 +0200

    Add files via upload

---
+++ UNO Classi.py
@@ -0,0 +1,11 @@
+import random
+
+class Carte:
+
+    def __init__(self, mazzo):
+
+        self._mazzo = mazzo
+        self._mano1 = list()
+        self._mano2 = list()
+        self._scarti = list()
+        
